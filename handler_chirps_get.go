70e5c53882df2600ebfab4a76b688bab30233aa0
cocuum <jonrjolley@gmail.com>
2026-07-30 18:03:52 -0700

    Added author filter to chirps

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,31 +2,10 @@ package main
 
 import (
 	"net/http"
-	
+
+	"github.com/cocuum/chirpy/internal/database"
 	"github.com/google/uuid"
 )
-
-func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "could not get chirps", err)
-		return
-	}
-
-	var allChirps = []Chirp{}
-	for _, chirp := range dbChirps {
-
-		allChirps = append(allChirps, Chirp{
-			ID:        chirp.ID,
-			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,
-			Body:      chirp.Body,
-			UserID:    chirp.UserID,
-		})
-	}
-
-	respondWithJSON(w, http.StatusOK, allChirps)
-}
 
 func (cfg *apiConfig) handlerGetChirpByID(w http.ResponseWriter, r *http.Request) {
 	type responseBody struct {
@@ -54,4 +33,52 @@ func (cfg *apiConfig) handlerGetChirpByID(w http.ResponseWriter, r *http.Request
 			UserID:    dbChirp.UserID,
 		},
 	})
+}
+
+func authorID(r *http.Request) (uuid.UUID, error) {
+	s := r.URL.Query().Get("author_id")
+	if s == "" {
+		return uuid.Nil, nil
+	}
+	authorID, err := uuid.Parse(s)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return authorID, nil
+}
+
+func prepareResponsePayload(db []database.Chirp) []Chirp {
+	var allChirps = []Chirp{}
+	for _, chirp := range db {
+			allChirps = append(allChirps, Chirp{
+				ID:        chirp.ID,
+				CreatedAt: chirp.CreatedAt,
+				UpdatedAt: chirp.UpdatedAt,
+				Body:      chirp.Body,
+				UserID:    chirp.UserID,
+			})
+		}
+	return allChirps
+}
+
+func (cfg *apiConfig) handlerGetAllChirps(w http.ResponseWriter, r *http.Request) {
+	authorID, err := authorID(r)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+	}
+
+	var dbChirps []database.Chirp
+	
+	if authorID != uuid.Nil {
+		dbChirps, err = cfg.db.GetChirpByUserID(r.Context(), authorID)
+	} else {
+		dbChirps, err = cfg.db.GetAllChirps(r.Context())
+	}
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "could not retrieve chirps", err)
+	}
+
+	chirps := prepareResponsePayload(dbChirps)
+
+	respondWithJSON(w, http.StatusOK, chirps)
 }
